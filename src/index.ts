4c5360502ceeeaacb6a886c737d8095cbe8a70a2
Hychtclyff <yudriqul20803@gmail.com>
2025-10-11 10:02:20 +0700

    chore(deploy): add fix configuration for Render

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ const host = process.env.HOST_LOCAL_AREA || 'localhost';
 try {
   server.listen(Number(port), () => {
     console.log(`server is on http://${host}:${port}/api/v1`);
-    socketServer.listen(Number(port));
-    // tidak diperlukan karena sudah terpasang pada 'server' http.
+    // Pemanggilan socketServer.listen() ini tidak diperlukan dan bisa dihapus.
   });
   startDataCheckCron();
   startPredictionRunnerCron(); // BARU: Aktifkan cron job prediksi
